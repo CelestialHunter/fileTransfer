8d95b95807af072e169da704f1921092ce74dd4c
Andrei - Petru Balea <andrei.balea@student.upt.ro>
2022-12-25 02:29:52 +0200

    Server - Client implementation + readme.md

--- client.c
+++ client.c
@@ -1,1 +1,156 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netdb.h>
+
+#define MY_PORT_ID 8008 /* numar > 5000 */
+
+int sockid;
+
+char* getServHostAddr()
+{
+    char hostbuffer[256];
+    struct hostent *host_entry;
+
+    gethostname(hostbuffer, sizeof(hostbuffer));
+    host_entry = gethostbyname(hostbuffer);
+
+    return inet_ntoa(*((struct in_addr*)host_entry->h_addr_list[0]));
+}
+
+int createConnection()
+{
+    // host IP
+    char* servHostAddr = getServHostAddr();
+    struct sockaddr_in ssock_addr;
+
+    // create socket
+    if ((sockid = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("Eroare creare socket.\n");
+        exit(-1);
+    }
+
+    // create ssock_addr
+    bzero((char *) &ssock_addr, sizeof(ssock_addr));
+    ssock_addr.sin_family = AF_INET;
+    ssock_addr.sin_port = htons(MY_PORT_ID);
+
+    if(inet_pton(AF_INET, servHostAddr, &ssock_addr.sin_addr) <= 0)
+    {
+        printf("\nAdresa invalida!\n");
+        exit(-1);
+    }
+
+    // connect to socket
+    if (connect(sockid, (struct sockaddr *) &ssock_addr, sizeof(ssock_addr)) < 0)
+    {
+        printf("Eroare conectare la server.\n");
+        exit(-1);
+    }
+}
+
+void closeConnection()
+{
+    close(sockid);
+}
+
+void printHelp()
+{
+    printf("===========================\n");
+    printf("Usage: \n");
+    printf("\"client [help]\" - displays this page\n");
+    printf("\"client ls\" - lists files on server directory\n");
+    printf("\"client get <filename>\" - downloads file from server\n");
+    printf("===========================\n");
+}
+
+void getList()
+{
+    char list[1024];
+    char cmd[] = "ls";
+    if ( (write(sockid, &cmd, sizeof(cmd))) < 0)
+    {
+        printf("Eroare scriere in socket");
+        exit(-1);
+    }
+
+    bzero((char *) &list, sizeof(list));
+    if ( (read(sockid, &list, sizeof(list))) < 0)
+    {
+        printf("Eroare citire din socket");
+        exit(-1);
+    }
+
+    printf("===========================\n");
+    printf("Lista:\n");
+    printf("===========================\n");
+    printf("%s\n", list);
+    printf("===========================\n");
+}
+
+void downloadFile(char* fileName)
+{
+    char fn[255];
+    strcpy(fn, fileName);
+    // TODO
+    if ( (write(sockid, &fn, sizeof(fn))) < 0)
+    {
+        printf("Eroare scriere in socket");
+        exit(-1);
+    }
+}
+
+
+int main(int argc, char** argv)
+{
+    switch(argc)
+    {
+        case 1:
+            // display Help page
+            printHelp();
+            break;
+        case 2:
+            // check if command is "ls"
+            if(strcmp(argv[1], "ls") == 0)
+            {
+                createConnection();
+                getList();
+                closeConnection();
+            }
+            else if(strcmp(argv[1], "help") == 0)
+            {
+                printHelp();
+            }
+            else
+            {
+                printf("Unknown command! Run \"client help\" for help.\n");
+                return -1;
+            }
+            break;
+        case 3:
+            if(strcmp(argv[1], "get") == 0)
+            {
+                createConnection();
+                downloadFile(argv[2]);
+                closeConnection();
+            }
+            else
+            {
+                printf("Unknown command! Run \"client help\" for help.\n");
+                return -1;
+            }
+            break;
+        default:
+            printf("Unknown command! Run \"client help\" for help.\n");
+            return -1;
+            break;
+    }   
+
+    return 0;
+}
--- server.c
+++ server.c
@@ -2,17 +2,122 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/wait.h>
-#include <signal.h>
+#include <sys/time.h>
 #include <errno.h>
-#include <sys/stat.h>
+#include <unistd.h>
+#include <strings.h> // bzero
+#include <signal.h>
+#include <stdlib.h>
+#include <fcntl.h> // open file
+#include <string.h> // strcmp
+#define MY_PORT_ID 8008 /* a number > 5000 */
 
-#define PORT 1337
+int sockid;
+int running = 1;
+
+char* srcDir = "server_source";
+
+void createConnection()
+{
+    struct sockaddr_in ssock_addr;
+    char msg[255];
+
+    sockid = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockid < 0)
+    {
+        perror("Eroare creare socket");
+        exit(-1);
+    }
+
+    bzero((char *) &ssock_addr, sizeof(ssock_addr));
+    ssock_addr.sin_family = AF_INET;
+    ssock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    ssock_addr.sin_port = htons(MY_PORT_ID);
+
+    if (( bind(sockid, (struct sockaddr *) &ssock_addr, sizeof(ssock_addr)) < 0))
+    {
+        perror("Eroare de asociere");
+        exit(-1);
+    }
+
+    if ( listen(sockid, 1) < 0)
+    {
+        perror("Eroare la ascultare");
+        exit(-1);
+    }
+}
+
+void closeConnection()
+{
+    close(sockid);
+}
+
+void intHandler(int sig)
+{
+    closeConnection();
+    running = 0;
+}
+
+void sendList(int destSockId)
+{
+    char list[1024];
+    char cmd[64];
+    sprintf(cmd, "ls %s > temp.txt", srcDir);
+    system(cmd);
+
+    int listFile = open("temp.txt", O_RDONLY);
+    bzero((char*)&list, sizeof(list));
+    read(listFile, list, sizeof(list));
+
+    close(listFile);
+    remove("temp.txt");
+
+    if(strlen(list) == 0)
+        sprintf(list, "Directory is empty!\n");
+
+    write(destSockId, list, sizeof(list));
+}
+
+void sendFile(int destSockId, char* fileName)
+{
+ // TODO
+}
+
+int main()
+{
+    int newsockid;
+    char msg[255];
+
+    createConnection();   
+
+    while (running)
+    {
+        newsockid = accept(sockid, (struct sockaddr *) 0, (int *) 0);
+        if (newsockid < 0)
+        {
+            perror("Eroare acceptare client");
+            return -1;
+        }
+        if ((read(newsockid, &msg, sizeof(msg))) < 0)
+        {
+            perror("Eroare citire din socket");
+            return -1;
+        }
+
+        printf("Mesaj primit: %s\n", msg);
+
+        if(strcmp(msg, "ls") == 0)
+        {
+            sendList(newsockid);
+        }
+        else
+        {
+            sendFile(newsockid, msg);
+        }
 
 
-
+        close(newsockid);
+    }
+    return 0;
+} 
